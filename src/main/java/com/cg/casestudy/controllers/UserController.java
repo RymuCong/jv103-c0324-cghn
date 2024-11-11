e20ff2239a824d35fb10e6011fa5cfa85f9ed4c2
Huyquang0519 <huyquang0519@gmail.com>
2024-11-11 20:41:57 +0700

    update login and register

---
+++ src/main/java/com/cg/casestudy/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.cg.casestudy.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+
+    @GetMapping("/home")
+    public String showHomePage() {
+        return "home";
+    }
+}
